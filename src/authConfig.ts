bcff12756f06eb3db544cc40eaf446c0f902b64c
Shaikh Afzaal <shaikhafzaal2@gmail.com>
2023-02-22 13:11:50 +0530

    corrected redirect uri in authconfig

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -9,7 +9,7 @@ export const msalConfig: Configuration = {
     auth: {
         clientId: "d1d0db53-bf5b-4565-a191-5ddd0914ee32",
         authority: "https://login.microsoftonline.com/e038180b-0021-401f-83a9-a2d45acee0dc",
-        redirectUri: "http://localhost:3000/"
+        redirectUri: location.hostname === "localhost"? "http://localhost:3000/": 'https://shaikhafzaal2.github.io/RPP-Frontend/',
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
